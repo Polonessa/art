2499624e632c2e738d1f62de41c3d6a04e83f6d6
Polonessa <logounova.polina@gmailcom>
2024-04-20 21:45:00 +0300

    "Лабораторная работа 8"

---
+++ front/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {createBrowserHistory} from "history";
+
+import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavigationBar />
+                <div className="container-fluid">
+                    <Routes>
+                         <Route path="/home" element={<Home/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
+/*function mapStateToProps(state) {
+    const { msg } = state.alert;
+    return { error_message: msg };
+}
+
+export default connect(mapStateToProps)(App);*/
+export default App;
